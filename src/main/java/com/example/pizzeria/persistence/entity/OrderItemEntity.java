ca37b7e35edacba2d0a2803251befc57708631ba
matiassbp <matiasbenonip@gmail.com>
2023-11-24 11:28:21 -0300

    Update OrderItemEntity

--- src/main/java/com/example/pizzeria/persistence/entity/OrderItemEntity.java
+++ src/main/java/com/example/pizzeria/persistence/entity/OrderItemEntity.java
@@ -8,12 +8,11 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "order_item")
+@IdClass(OrderItemId.class)
 @Getter
 @Setter
 @NoArgsConstructor
 public class OrderItemEntity {
-
-
     @Id
     @Column(name = "id_order", nullable = false)
     private Integer idOrder;
@@ -22,13 +21,20 @@ public class OrderItemEntity {
     @Column(name = "id_item", nullable = false)
     private Integer idItem;
 
-
     @Column(name = "id_pizza", nullable = false)
     private Integer idPizza;
 
-    @Column(nullable = false, columnDefinition = "DECIMAL(2,1)")
+    @Column(nullable = false, columnDefinition = "Decimal(2,1)")
     private Double quantity;
 
-    @Column(nullable = false, columnDefinition = "DECIMAL(5,2)")
+    @Column(nullable = false, columnDefinition = "Decimal(5,2)")
     private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "id_order", referencedColumnName = "id_order", insertable = false, updatable = false)
+    private OrderEntity order;
+
+    @OneToOne
+    @JoinColumn(name = "id_pizza", referencedColumnName = "id_pizza", insertable = false, updatable = false)
+    private PizzaEntity pizza;
 }
